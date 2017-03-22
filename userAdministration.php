6465d87c7ffa07de369282e0e05a7a2b98b14b22
Fahri Can <fahri-can@gmx.at>
2017-03-22 12:20:07 +0100

    Create userAdministration.php

---
+++ userAdministration.php
@@ -0,0 +1,20 @@
+<?php
+$userObjekte = $db->getUserList();
+?>
+
+<form>
+    <table class="table table-striped">
+        <tr>
+            <th>Anrede</th>
+            <th>Vorname</th>
+            <th>Nachname</th>
+            <th>Adresse</th>
+            <th>PLZ</th>
+            <th>Ort</th>
+            <th>Username</th>
+            <th>Passwort</th>
+            <th>Email</th>
+            <th colspan="2">Funktion</th>
+        </tr>
+    </table>
+</form>
